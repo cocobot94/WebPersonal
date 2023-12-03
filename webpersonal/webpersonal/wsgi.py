7d999d78b28854e0f193ee9ee5123ac0f46e71b3
“cocobot94” <elierjerez@email.com>
2023-12-03 15:25:29 -0500

    aa

--- webpersonal/webpersonal/wsgi.py
+++ webpersonal/webpersonal/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webpersonal.settings')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webpersonal.settings")
 
 application = get_wsgi_application()
