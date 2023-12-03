fedad29d5ec337e262cdf119d26eafc131e6f02c
“cocobot94” <elierjerez@email.com>
2023-12-03 16:06:07 -0500

    cfsdcsa

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +79,9 @@ WSGI_APPLICATION = "webpersonal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
+# Configuración de la base de datos de Heroku
+DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
